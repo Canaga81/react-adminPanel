0aec373ef07d5209fe0bb69c64ea88a96abfc806
Canaga81 <Canagamemmedov@gmail.com>
2024-05-04 14:26:39 +0400

    Make it responsive and Finish Project

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -2,52 +2,65 @@ import "./Sidebar.css";
 import Logo from "../../imgs/logo.png";
 import { SidebarData } from "../../Data/Data";
 import { FaRegCircleRight } from "react-icons/fa6";
+import { FaBars } from "react-icons/fa";
 import { useState } from "react";
+import { motion } from "framer-motion";
 
 const Sidebar = () => {
+  const [selected, setSelected] = useState(0);
+  const [expanded, setExpanded] = useState(true);
 
-    const [selected, setSelected] = useState(0);
-
+  const sidebarVariants = {
+    true: {
+      left: '0'
+    },
+    false: {
+      left: '-60%'
+    }
+  }
 
   return (
-
-    <div className="Sidebar">
-
-      {/* Logo */}
-      <div className="logo">
-
-        <img src={Logo} alt="" />
-
-        <span>
-          Sh<span>o</span>ps
-        </span>
-
+    <>
+      <div 
+      className="bars" style={expanded ? { left: "60%" } : { left: "5%" }}
+      onClick={() => setExpanded(!expanded)}
+      >
+        <FaBars />
       </div>
 
-      {/* Menu */}
-      <div className="menu">
+      <motion.div
+      variants={sidebarVariants}
+      animate={window.innerWidth <= 768 ? `${expanded}` : ''}
+      className="Sidebar">
+        {/* Logo */}
+        <div className="logo">
+          <img src={Logo} alt="" />
 
-        {SidebarData.map((item, index) => (
-
-          <div onClick={() => setSelected(index)} key={index} className={selected === index ? `menuItem active` : `menuItem`}>
-
-            <item.icon/>
-            <span>{item.heading}</span>
-
-          </div>
-
-        ))}
-
-        <div className="menuItem">
-            <FaRegCircleRight />
+          <span>
+            Sh<span>o</span>ps
+          </span>
         </div>
 
-      </div>
+        {/* Menu */}
+        <div className="menu">
+          {SidebarData.map((item, index) => (
+            <div
+              onClick={() => setSelected(index)}
+              key={index}
+              className={selected === index ? `menuItem active` : `menuItem`}
+            >
+              <item.icon />
+              <span>{item.heading}</span>
+            </div>
+          ))}
 
-    </div>
-
+          <div className="menuItem">
+            <FaRegCircleRight />
+          </div>
+        </div>
+      </motion.div>
+    </>
   );
-
 };
 
 export default Sidebar;
